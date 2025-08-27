87c5de4f8a92ceb5a088d6c3f992ed4103b92d5e
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-27 14:32:55 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -132,7 +132,7 @@ fdk.handle(async function(input, ctx){
 		//jiraClientId      = await getSecret('jira-ClientId');
 		//jiraClientSecret  = await getSecret('jira-ClientSecret');
 		//return {"ociCompartmentName": getCompartmentOCID(ociCompartmentName)};
-		return {"ociCompartmentName": "duno"};
+		return {"ociCompartmentName": ociCompartmentName};
 	} catch (error) {
 		console.error('Error fetching data:', error);
 		return error;
