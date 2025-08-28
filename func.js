752c2f3c4b2bcfe949f817e9f9878e5449389472
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-28 18:23:56 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -9,13 +9,16 @@ fdk.handle(async function(input, ctx){
 	//try {
 		let jiraClientId	= 0;
 		let jiraClientSecret= 0;
-		const provider 			  = new common.ResourcePrincipalAuthenticationDetailsProvider(); // using resource principal provider of OCI
-		//const identityClient 	  = new identity.IdentityClient({authenticationDetailsProvider: provider});
+		const provider:ResourcePrincipalAuthenticationDetailsProvider = new common.ResourcePrincipalAuthenticationDetailsProvider(); // using resource principal provider of OCI
+		//(async () => {
+		//const identityClient 	  = await new identity.IdentityClient({authenticationDetailsProvider: provider});
+		
 		//const provider 			= new common.ResourcePrincipalAuthenticationDetailsProvider();
         //const identityClient 	= new identity.IdentityClient({ authenticationDetailsProvider: provider });
 		const ociVaultName        = "IBA Vault";
 		const ociCompartmentName  = "NONPROD-Oracle-Integration-CMP";	
-
+		
+		//})();
 		return {"tenantId": provider.getTenantId()};
 	//} catch (error) {
 	//	console.error('Error fetching data:', error);
