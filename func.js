ec5fad395bd640db9610a1accd75b95f3133762d
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-28 09:30:09 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,10 +1,22 @@
-const fdk=require('@fnproject/fdk');
+const fdk			= require('@fnproject/fdk');
+const common        = require("oci-common");
+const secrets       = require("oci-secrets");
+const vaults        = require("oci-vault");
+const identity      = require("oci-identity");
+const keymanagement = require("oci-keymanagement");
 
-fdk.handle(function(input, ctx){
-  let name = 'World';
-  if (input.name) {
-    name = input.name;
-  }
-  console.log(ctx.httpGateway);
-  return {"message":"hello world"}
+fdk.handle(async function(input, ctx){
+	try {
+		let jiraClientId	= 0;
+		let jiraClientSecret= 0;
+		const provider 			  = new common.ResourcePrincipalAuthenticationDetailsProvider(); // using resource principal provider of OCI
+		const identityClient 	  = new identity.IdentityClient({authenticationDetailsProvider: provider});
+		const ociVaultName        = "IBA Vault";
+		const ociCompartmentName  = "NONPROD-Oracle-Integration-CMP";	
+
+		return {"tenantId": "new"};
+	} catch (error) {
+		console.error('Error fetching data:', error);
+		return error;
+	}
 })
