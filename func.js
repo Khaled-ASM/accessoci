e2e15e2a779e99cf1e9bd54b2f5efca757de7ab9
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-28 09:18:19 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,8 +1,10 @@
+const fdk=require('@fnproject/fdk');
+
 fdk.handle(function(input, ctx){
   let name = 'World';
   if (input.name) {
     name = input.name;
   }
   console.log(ctx.httpGateway);
-  return {"ctx":ctx.httpGateway}
+  return {"message":"hello world"}
 })
